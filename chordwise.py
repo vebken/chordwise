b87bd501513187d8928d0c0364d5685233bb9dfa
Victor Ebken <vebken@splunk.com>
2016-08-22 00:32:05 -0700

    More development, adding zipped sound library to remote

--- chordwise.py
+++ chordwise.py
@@ -21,42 +21,43 @@ def ignore_stderr():
         os.close(old_stderr)
 
 with ignore_stderr():
-  #pygame.mixer.pre_init(22050, -16, 2, 2048)
-  pygame.mixer.pre_init(44100, -16, 2, 100)
-  pygame.mixer.init()
-  pygame.mixer.set_num_channels(128)
+    #pygame.mixer.pre_init(22050, -16, 2, 2048)
+    pygame.mixer.pre_init(44100, -16, 2, 100)
+    pygame.mixer.init()
+    pygame.mixer.set_num_channels(128)
 
 # Constants
 
 GRAND_PIANO = 'GRAND_PIANO'
-NOTES = ['A','A_SHARP','B','C','C_SHARP', 'D','D_SHARP','E','F','F_SHARP','G','G_SHARP']
+NOTES = ['C','C_SHARP', 'D','D_SHARP','E','F','F_SHARP','G','G_SHARP','A','A_SHARP','B']
 CHORD_SCALES = ['Major', 'Minor','Note']
 CHORD_FORMS = ['Triad', 'Seventh']
 #TODO create dictionary to map these to the constants so that the conversion doesn't have to happen in the UI file
 
 
-A_FLAT = 11
-A = 0
-A_SHARP = 1
-B_FLAT = 2
-B = 2
-C = 3
-C_SHARP = 4
-D_FLAT = 4
-D = 5
-D_SHARP = 6
-E_FLAT = 6
-E = 7
-F = 8
-F_SHARP = 9
-G_FLAT = 9
-G = 10
-G_SHARP = 11
+A_FLAT = 8
+A = 9
+A_SHARP = 10
+B_FLAT = 10
+B = 11
+C = 0
+C_SHARP = 1
+D_FLAT = 1
+D = 2
+D_SHARP = 3
+E_FLAT = 3
+E = 4
+F = 5
+F_SHARP = 6
+G_FLAT = 6
+G = 7
+G_SHARP = 8
 
 WAV = '.WAV'
 SHARP = 'SHARP'
 # Need to start at 1 so we don't jump around (left of keyboard)
-OCTAVES = ['1','2','3','4','5','6','7','8']
+# TODO Fixing starting at 1 now, making it C based instead of A based should fix
+OCTAVES = ['0','1','2','3','4','5','6','7','8']
 HALF = 1
 WHOLE = 2
 MAJOR_SCALE = [WHOLE, WHOLE, HALF, WHOLE, WHOLE, WHOLE, HALF]
@@ -69,104 +70,123 @@ TRIAD_2ND_INV = [4, 7, 9] # DOUBLE CHECK THIS
 SEVENTH = [0, 2, 4, 6]
 SEVENTH_1ST_INV = [2, 4, 6, 7]
 
-
+'''
+# old sound library
 piano_notes = []
 for note in NOTES:
-  note_list = []
-  for octave in OCTAVES:
-    path = os.path.join('grand_piano', note, 'notes', note + "_" + octave + WAV)
-    if (os.path.exists(path)):
-      sound = pygame.mixer.Sound(path)
-      note_list.append(sound)
-  piano_notes.append(note_list)
-
+    note_list = []
+    for octave in OCTAVES:
+        path = os.path.join('grand_piano', note, 'notes', note + "_" + octave + WAV)
+        if (os.path.exists(path)):
+            sound = pygame.mixer.Sound(path)
+            note_list.append(sound)
+    piano_notes.append(note_list)
+'''
+# new sound library
+piano_notes = []
+piano_sounds = []
+for octave in OCTAVES:
+    note_list = []
+    sound_list = []
+    for note in NOTES:
+        path = os.path.join('grand_piano', octave + '_' + note + WAV)
+        if os.path.exists(path):
+            print path
+            note_list.append(note)
+            sound = pygame.mixer.Sound(path)
+            sound_list.append(sound)
+    piano_notes.append(note_list)
+    piano_sounds.append(sound_list)
 
 # Sharp a note
 def sharp(note):
-  return (note + 1) % 12
+    return (note + 1) % 12
 
 # Flat a note
 def flat(note):
-  note = note - 1
-  return note if note > 0 else 11
+    note = note - 1
+    return note if note > 0 else 11
 
 # Find note that is interval steps higher
 def find_interval(note, interval):
-  return (((note[0] + interval) % 12), note[1] if (note[0] + interval) < 12 else note[1] + 1)
+    # note = ((note[0] + interval) % 12)
+    # octave = note[1] if (note[0] + interval) < 12 else note[1] + 1
+    return (((note[0] + interval) % 12), note[1] if (note[0] + interval) < 12 else note[1] + 1)
 
 # TODO add optional scale parameter to prevent need to construct
 # TODO also add optional positional parameter to use for chord progressions
 def construct_chord(root_note, form, chord_form):
-  scale = construct_scale(root_note, form)
-  chord = []
-  for pos in chord_form:
-    chord.append(scale[pos])
-  return chord
-  
+    scale = construct_scale(root_note, form)
+    chord = []
+    for pos in chord_form:
+        chord.append(scale[pos])
+    return chord
+
 
 # Construct a scale given a note, it's octave, and the scale form (a list of intervals)
 def construct_scale(root_note, form, num=8):
-  scale = []
-  cur_note = root_note
-  scale.append(cur_note)
-  # keeps track of how big of a scale desired
-  # TODO ?  might be able to ditch this for logic that increments octaves.
-  count = 1
-  for interval in form:
-    if count == num:
-      break
-    cur_note = find_interval(cur_note, interval)
+    scale = []
+    cur_note = root_note
     scale.append(cur_note)
-    count += 1
-  return scale
+    # keeps track of how big of a scale desired
+    # TODO ?  might be able to ditch this for logic that increments octaves.
+    count = 1
+    for interval in form:
+        if count == num:
+            break
+        cur_note = find_interval(cur_note, interval)
+        scale.append(cur_note)
+        count += 1
+    return scale
 
 def find_chord(notes):
-  num = notes.length()
-  # starting from root note
-  # construct scale of root note
+    num = notes.length()
+    # starting from root note
+    # construct scale of root note
 
 # Return true if note is accidental
 def isAccidental(name):
-  if sharp in name:
-    return True 
+    if sharp in name:
+        return True
 
-# Return true if note is natural
+    # Return true if note is natural
 def isNatural(name):
-  if sharp in name:
-    return False
-  
+    if sharp in name:
+        return False
+
 
 def play(path, duration):
-  print ('play path is %s' % path)
-  sound = pygame.mixer.Sound(path)
-  mili = int(duration * 1000)
-  sound.play(maxtime=mili)
+    print ('play path is %s' % path)
+    sound = pygame.mixer.Sound(path)
+    mili = int(duration * 1000)
+    sound.play(maxtime=mili)
 
 def play_note(note, instrument, duration, solo=True):
-  path = os.path.join(instrument,NOTES[note[0]], "notes", NOTES[note[0]], "_", str(note[1]), WAV)
-  print path
-  play(instrument + '/' + NOTES[note[0]] + '/notes/' + NOTES[note[0]] + '_' + str(note[1]) + WAV, duration)
+    path = os.path.join(instrument,NOTES[note[0]], "notes", NOTES[note[0]], "_", str(note[1]), WAV)
+    print path
+    play(instrument + '/' + NOTES[note[0]] + '/notes/' + NOTES[note[0]] + '_' + str(note[1]) + WAV, duration)
 #  if solo:
 #    time.sleep(duration)
 
-def play_from_mem(note, instrument, duration, solo=True):
-  mili = int(duration * 1000)
-  # note-1 for octave since octaves are 1 based, but array is 0 based
-  if (len(piano_notes[note[0]]) >= note[1]):
-      piano_notes[note[0]][note[1]-1].play(maxtime=mili)
-  else:
-    print ('octave %s for note %s is out of range' % (str(note[1]), NOTES[note[0]]))
-#  if solo:
-#    time.sleep(duration)
+def play_from_mem(note, instrument, duration):
+    mili = int(duration * 1000)
+    # if note exists in octave's notes
+    if (NOTES[note[0]] in piano_notes[note[1]]):
+        #find correct index, octaves 0 and 8 are misleading
+        note_idx = piano_notes[note[1]].index(NOTES[note[0]])
+        piano_sounds[note[1]][note_idx].play(maxtime=mili)
+    else:
+        print ('octave %s for note %s is out of range' % (str(note[1]), NOTES[note[0]]))
+
 
 def play_chord(root_note, scale_type, chord_type, instrument, duration):
-  #calculate chord
-  chord = construct_chord(root_note, scale_type, chord_type)
-  for note in chord:
-    #print note
-    #play_note(note, instrument, duration, solo=False)
-    play_from_mem(note, instrument, duration, solo=False)
-  #time.sleep(duration)
+    #calculate chord
+    chord = construct_chord(root_note, scale_type, chord_type)
+    for note in chord:
+        print note
+        #play_note(note, instrument, duration)
+        play_from_mem(note, instrument, duration)
+        #time.sleep(duration)
 
 
 #TODO start up page. add text drop downs for chords, scales, notes,
--- ui.py
+++ ui.py
@@ -66,6 +66,41 @@ class ChordScaleListBox(wx.ListBox):
     def OnSelect(self, e):
         e.Skip()
 
+#def populateScale(self):
+
+def playButtonExec(self):
+    note = self.NoteListBox.GetSelection()
+    octave = int(self.OctaveListBox.GetStringSelection())
+    form = []
+    scale = []
+    # TODO make this logic come from dictionaries that have list of options mapped to choices in listbox
+    if (self.ChordScaleListBox.GetStringSelection() == 'Note'):
+        chordwise.play_from_mem((note, octave), GRAND_PIANO, self.spinctrl.GetValue())
+    else:
+        if (self.ChordScaleListBox.GetStringSelection() == 'Minor'):
+            scale = MINOR_SCALE
+        else:
+            scale = MAJOR_SCALE
+
+        if (self.ChordFormListBox.GetStringSelection() == 'Triad'):
+            form = TRIAD
+        else:
+            form = SEVENTH
+
+        play_chord((note, octave), scale, form, GRAND_PIANO, self.spinctrl.GetValue())
+
+
+#def constructScaleExec(self):
+
+def initScaleText(self, mpnl):
+    self.pos_1 = wx.StaticText(mpnl, -1, "1", (50,0), style=wx.ALIGN_CENTRE)
+    #self.pos_2 = wx.StaticText(mpnl, -1, "2")
+    #self.pos_3 =
+    #self.pos_4
+    #self.pos_5
+    #self.pos_6
+    #self.pos_7
+
 class HomePage(wx.Frame):
            
     def __init__(self, *args, **kw):
@@ -81,12 +116,15 @@ class HomePage(wx.Frame):
         PlayButton(mpnl, label='Play', pos=(15, 15))
         self.ScaleCreatorButton = wx.Button(mpnl, label='Construct Scale', pos=(15,200))
         self.NoteListBox = NoteListBox(mpnl, 26, (200,0), (100, 245), NOTES, wx.LB_SINGLE)
-        self.OctaveListBox = OctaveListBox(mpnl, 27, (300,0), (100, 165), OCTAVES, wx.LB_SINGLE)
+        self.OctaveListBox = OctaveListBox(mpnl, 27, (300,0), (100, 185), OCTAVES, wx.LB_SINGLE)
         self.ChordScaleListBox = ChordFormListBox(mpnl, 28, (400,0), (100, 130), CHORD_SCALES, wx.LB_SINGLE)
         self.ChordFormListBox = ChordFormListBox(mpnl, 29, (500,0), (100, 130), CHORD_FORMS, wx.LB_SINGLE)
         self.Bind(wx.EVT_BUTTON, self.OnButtonClicked)
+        #TODO initialize all text boxes required for scales
+        initScaleText(self,mpnl)
+        #self.i
         self.spinctrl = wx.SpinCtrl(mpnl, 30, '3', (120, 20), (60, -1), wx.SP_ARROW_KEYS, 1, 10, 3)
-
+        self.png = wx.StaticBitmap(mpnl, -1, wx.Bitmap("88_key.png", wx.BITMAP_TYPE_ANY), pos=(15,300))
         self.SetTitle('Chordwise')
         self.Centre()
         self.Show(True)  
@@ -94,33 +132,19 @@ class HomePage(wx.Frame):
 
     def OnButtonClicked(self, e):
         button = e.GetEventObject()
-        print button.GetLabel()
+        label = button.GetLabel()
+        if (label == "Play"):
+            playButtonExec(self)
+        elif (label == "Construct Scale"):
+            constructScaleExec(self)
         # GetSelection returns 0 based integer position
-        note = self.NoteListBox.GetSelection()
-        octave = int(self.OctaveListBox.GetStringSelection())
-        form = []
-        scale = []
-        #TODO make this logic come from dictionaries that have list of options mapped to choices in listbox
-        if (self.ChordScaleListBox.GetStringSelection() == 'Note'):
-            chordwise.play_from_mem((note, octave), GRAND_PIANO, self.spinctrl.GetValue())
-        else:
-            if (self.ChordScaleListBox.GetStringSelection() == 'Minor'):
-                scale = MINOR_SCALE
-            else:
-                scale = MAJOR_SCALE
 
-            if (self.ChordFormListBox.GetStringSelection() == 'Triad'):
-                form = TRIAD
-            else:
-                form = SEVENTH
-
-            play_chord((note,octave),scale,form,GRAND_PIANO,self.spinctrl.GetValue())
 
 
 def main():
     
     ex = wx.App()
-    HomePage(None,size=(600,350))
+    HomePage(None,size=(1273,485))
     ex.MainLoop()    
 
 
